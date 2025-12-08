5b5c44e98d202b1c3f8e90bb3f9c9994f87d00ab
hyeongu01 <cdy5212@gmail.com>
2025-12-09 02:51:54 +0900

    feat: update /src/repositories

--- src/repositories/author.repository.js
+++ src/repositories/author.repository.js
@@ -7,7 +7,7 @@ module.exports = {
     },
 
     // r
-    findAll({options = {}, transaction = null} = {}) {
+    findAll(options = {}, transaction = null) {
         return Author.findAll({...options, transaction});
     },
 
