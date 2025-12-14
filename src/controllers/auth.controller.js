c515ffd8177cd4e6d68813208abac2cfa0317b58
hyeongu01 <cdy5212@gmail.com>
2025-12-14 15:51:11 +0900

    feat: update error throw

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -5,25 +5,23 @@ const NotFoundError = require("../errors/NotFound");
 module.exports = {
     // 회원가입
     async signup(req, res) {
-        // 입력 검증
-        const {username, password, email, name, phone} = req.body;
-        
-        if (!username) {
-            const error = new BadRequestError("username 이 없습니다.");
-            return res.status(error.statusCode).json(error.response())
-        }
-
-        if (!password) {
-            const error = new BadRequestError("password 가 없습니다.");
-            return res.status(error.statusCode).json(error.response())
-        }
-
-        if (!name) {
-            const error = new BadRequestError("name 이 없습니다.");
-            return res.status(error.statusCode).json(error.response());
-        }
-
         try {
+            // input validation
+            const {username, password, email, name, phone} = req.body;
+            
+            if (!username) {
+                throw new BadRequestError("username 이 없습니다.");
+            }
+
+            if (!password) {
+                throw new BadRequestError("password 가 없습니다.");
+            }
+
+            if (!name) {
+                throw new BadRequestError("name 이 없습니다.");
+            }
+
+            // processing
             const user = await authService.signup({
                 username,
                 password,
@@ -42,19 +40,21 @@ module.exports = {
     // 로그인
     async login(req, res) {
         // 입력 검증
-        const {username, password} = req.body;
 
-        if (!username) {
-            const error = new BadRequestError("username 이 없습니다.");
-            return res.status(error.statusCode).json(error.response());
-        }
-
-        if (!password) {
-            const error = new BadRequestError("password 이 없습니다.");
-            return res.status(error.statusCode).json(error.response());
-        }
 
         try {
+            // input validation
+            const {username, password} = req.body;
+
+            if (!username) {
+                throw new BadRequestError("username 이 없습니다.");
+            }
+
+            if (!password) {
+                throw new BadRequestError("password 이 없습니다.");
+            }
+
+            // processing
             const data = await authService.login({ username, password });
             return res.status(200).json({ data, meta: { timestamp: new Date() } });
         } catch (err) {
@@ -63,15 +63,17 @@ module.exports = {
     },
 
     async logout(req, res) {
-        // refresh Token 이 있는지 검증
-        const {refreshToken} = req.body;
 
-        if (!refreshToken) {
-            const error = new BadRequestError("refreshToken 필드 누락");
-            return res.status(error.statusCode).json(error.response());
-        }
 
         try {
+            // input validation
+            const {refreshToken} = req.body;
+
+            if (!refreshToken) {
+                throw new BadRequestError("refreshToken 필드 누락");
+            }
+
+            // processing
             const data = await authService.logout(req.user, refreshToken);
             return res.status(200).json({ data, meta: { timestamp: new Date() } });
         } catch (err) {
@@ -80,14 +82,16 @@ module.exports = {
     },
 
     async refresh(req, res) {
-        const {refreshToken} = req.body;
-
-        if (!refreshToken) {
-            const error = new BadRequestError("refreshToken 필드 누락");
-            return res.status(error.statusCode).json(error.response());
-        }
-
         try {
+            // input validation
+            const {refreshToken} = req.body;
+
+            if (!refreshToken) {
+                const error = new BadRequestError("refreshToken 필드 누락");
+                return res.status(error.statusCode).json(error.response());
+            }
+
+            // processing
             const data = await authService.refresh(refreshToken);
             return res.status(201).json({ data, meta: { timestamp: new Date() } });
         } catch (err) {
